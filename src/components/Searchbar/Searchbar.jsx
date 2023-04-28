f2c2de85f7b1a8246c63380056e09157f8dfea6b
chemvic <chemvic79@gmail.com>
2023-04-28 21:12:03 +0300

    searchbar на хуки

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,38 +1,36 @@
-import React, {Component} from 'react';
+import {useState} from 'react';
 import PropTypes from 'prop-types';
 import css from "./Searchbar.module.css";
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { FaSearch } from "react-icons/fa";
 
-class Searchbar extends Component {
-  state={
-    imagesForSearch:'',
-  }
+
+const Searchbar = ({onSubmit})=> {
+  const[ imagesForSearch, setImagesForSearch ]=useState('');
  
-  handleSubmit=(event)=>{
+  const handleSubmit=(event)=>{
     
-    const{imagesForSearch}=this.state;
-     event.preventDefault();
+      event.preventDefault();
 
     if(imagesForSearch.trim()===''){
       toast.error('Enter images for search');  
          return;
     }   
-    this.props.onSubmit(imagesForSearch);
-    this.setState({imagesForSearch:''});
+    onSubmit(imagesForSearch);
+    setImagesForSearch('');
  
   }
 
-  handleImagesNames =(event)=>{
-    this.setState({imagesForSearch: event.currentTarget.value.toLowerCase()});
+  const handleImagesNames =(event)=>{
+    setImagesForSearch(event.currentTarget.value.toLowerCase());
     
   }
 
-  render(){
+ 
      return(
         <header className={css.searchbar}>
-  <form className={css.searchForm} onSubmit={this.handleSubmit}>
+  <form className={css.searchForm} onSubmit={handleSubmit}>
     <button type="submit" className={css.searchFormButton}>
         <FaSearch  style={{color: "#84868b",}}/>
     </button>
@@ -43,13 +41,13 @@ class Searchbar extends Component {
       autoComplete="off"
       autoFocus
       placeholder="Search images and photos"
-      onChange={this.handleImagesNames}
+      onChange={handleImagesNames}
     />
   </form>
 
 </header>
     )
-  }
+ 
    
 }
 
