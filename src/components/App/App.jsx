13b5b9e79eb376fddc8aa73c04cd3d9ff3f4c2ad
chemvic <chemvic79@gmail.com>
2023-04-28 23:06:05 +0300

    app на хуки

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import { useState, useEffect } from 'react';
 import Searchbar from "../Searchbar";
 import ImageGallery from "../ImageGallery";
 import css from "./App.module.css";
@@ -9,96 +9,96 @@ import Button from "../Button";
 import Loader from '../Loader';
 import imagesAPI from "../../api/fetchImages-api";
 
-class App extends Component {
+const App = ()=> {
 
-  state={
-    images:[],
-    imagesForSearch :'',
-    currentPage: 1,
-    showModal: false,
-    imageForModal: null,
-    tags:'',
-    isLoading:false, 
-    total: 0,
-    error: null, 
-  };
+  const[images, setImages]=useState([]);
+  const[ imagesForSearch, setImagesForSearch ]=useState('');
+  const[ currentPage, setCurrentPage ]=useState(1);
+  const[ showModal, setShowModal ]=useState(false);
+  const[ imageForModal, setImageForModal ]=useState(null);
+  const[ tags, setTags ]=useState('');
+  const[ isLoading, setIsLoading ]=useState(false);
+  const[ total, setTotal ]=useState(0);
+  const[ error, setError ]=useState(null);
 
-  async componentDidUpdate(_, prevState) { 
-    
-    if(prevState.imagesForSearch !== this.state.imagesForSearch||prevState.currentPage !== this.state.currentPage){
-     this.fetchImages();
-           
-}
-}
+
+  useEffect(()=>{
+
+    const fetchImages=async()=>{
+      setIsLoading(true);
+      try {
+        const newImages= await imagesAPI.fetchImagesWithQuery(imagesForSearch, currentPage);
+        
+        if (newImages.data.hits.length === 0) {
+          return toast.error(
+            "No images by your query"
+          );
+        }
+          setImages( images=>[...images, ...newImages.data.hits]);
+          setTotal(newImages.data.totalHits);
+        
+      } catch (error) {
+       setError(error);
+      } finally {
+       setIsLoading(false);    }
+    }
+
+    if(imagesForSearch===''){
+      return;
+    }
+
+    fetchImages();
+
+  },[ imagesForSearch, currentPage ]);
+
 
  
-  formSubmitHandler=(imagesForSearch)=>{     
-    this.setState({ imagesForSearch, images:[], currentPage:1 });
+  const formSubmitHandler=(imagesForSearch)=>{     
+    setImagesForSearch(imagesForSearch);
+    setImages([]);
+    setCurrentPage(1);
   }
 
-  fetchImages=async()=>{
-    const {currentPage, imagesForSearch}=this.state;
-    this.setState({ isLoading: true });
-    try {
-      const newImages= await imagesAPI.fetchImagesWithQuery(imagesForSearch, currentPage);
-      
-      if (newImages.data.hits.length === 0) {
-        return toast.error(
-          "No images by your query"
-        );
-      }
-      this.setState(({images}) => ({
-          images: [...images, ...newImages.data.hits],
-          total:  newImages.data.totalHits        
-        }));
-        
-      
-    } catch (error) {
-      this.setState({ error });
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  }
+ 
 
-  loadMoreImages =() => {   
-    this.setState(
-    (prevState) => ({ currentPage: prevState.currentPage + 1 })       
-  );
+ const loadMoreImages =() => {   
+ 
+      setCurrentPage(currentPage=>currentPage + 1);
  };
 
 
 
-  onOpenModal=(largeImageURL, tags)=>{
-this.setState({
-  imageForModal:largeImageURL,
-  showModal:true,
-  tags:tags
-  });
+ const onOpenModal=(largeImageURL, tags)=>{
+
+    setImageForModal(largeImageURL);
+    setShowModal(true);
+    setTags(tags);
   }
 
-  onCloseModal=()=>{
-    this.setState({imageForModal:null, showModal:false, tags:''});
+ const onCloseModal=()=>{
+        setImageForModal(null);
+        setShowModal(false);
+        setTags('');
   }
 
-render(){
-  const { images, showModal, error, total, imageForModal, tags, isLoading }=this.state;
+
   const totalPage = total / images.length;
    return (
     <div className={css.App}>   
     
-      <Searchbar onSubmit={this.formSubmitHandler}/>
+      <Searchbar onSubmit={formSubmitHandler}/>
       <ToastContainer autoClose={1000}/>
 
-      {images.length>0 &&     <ImageGallery images={images} onOpenModal={this.onOpenModal}/>
+      {images.length>0 &&     <ImageGallery images={images} onOpenModal={onOpenModal}/>
 }
       
 
       {(isLoading) && 
       (<Loader visible={true}/>)}
 {(images.length>0 && !isLoading && totalPage > 1 )&&
-<Button onClick={this.loadMoreImages}/>}
+<Button onClick={loadMoreImages}/>}
       {showModal && (
-          <Modal image={imageForModal} tags={tags} onClose={this.onCloseModal} />
+          <Modal image={imageForModal} tags={tags} onClose={onCloseModal} />
         )}
  {error && (
          <p>"Something went wrong((("</p>
@@ -106,6 +106,6 @@ render(){
 
     </div>
   );
-}
+
 };
 export default App;
